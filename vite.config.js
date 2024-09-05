eb14dc71baf7dbed628d79c1cd6fb42421ecc01a
badalsah <badalsah8541@gmail.com>
2024-09-05 14:02:24 +0530

    added base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 10000, // Port that Render expects
-    strictPort: true, // Ensures that Vite will fail if the port is already in use
-  },
-  build: {
-    outDir: 'build', // Ensure this matches your expected output directory
-  },
+  base:"/Lunacal/",
 })
