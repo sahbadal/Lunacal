7df47302360daf79750b31a4b88729cb02965a07
badalsah <badalsah8541@gmail.com>
2024-09-05 13:53:36 +0530

    vite config setup

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000, // Port that Render expects
+    strictPort: true, // Ensures that Vite will fail if the port is already in use
+  },
+  build: {
+    outDir: 'build', // Ensure this matches your expected output directory
+  },
 })
